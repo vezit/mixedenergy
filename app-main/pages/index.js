13d39fee506874ff5b77c7bac5c4c8d72106104a
Victor Reipur <victor.reipur@gmail.com>
2024-07-27 08:38:33 +0000

    initial commit

---
+++ app-main/pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+function HomePage() {
+    return (
+      <div className="flex justify-center items-center h-screen bg-blue-100">
+        <h1 className="text-5xl font-bold text-blue-500">
+          Hello World!
+        </h1>
+      </div>
+    );
+  }
+  
+  export default HomePage;
