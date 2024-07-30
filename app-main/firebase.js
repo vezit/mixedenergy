c677c8c6a667f9e86adf5160b745df373ff407b4
Victor Reipur <victor.reipur@gmail.com>
2024-07-30 20:31:30 +0000

    firebase and quickpay is connected

--- app-main/firebase.js
+++ app-main/firebase.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Ensure this import is present
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDa85TT8d8ZdWqMoOr7IYwaTFXkmgsCPhs",
-  authDomain: "mixedenergy-7d498.firebaseapp.com",
-  projectId: "mixedenergy-7d498",
-  storageBucket: "mixedenergy-7d498.appspot.com",
-  messagingSenderId: "575667819467",
-  appId: "1:575667819467:web:af0c0867049ca4659a1c4e",
-  measurementId: "G-35FLWQS86M"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
