7f520e259b612ffb1c25365b71b9ef49e635bcff
Victor Reipur <victor.reipur@gmail.com>
2024-09-22 20:56:17 +0000

    google analytics

--- app-main/pages/_app.js
+++ app-main/pages/_app.js
@@ -1,18 +1,33 @@
+// pages/_app.js
 import '../styles/globals.css';
-import Header from '../components/Header';  // Import the Header component
-import Footer from '../components/Footer';  // Import the Footer component
-import { useModal } from '../lib/modals';   // Import the useModal hook
-import CookieConsent from '../components/CookieConsent';  // Import the CookieConsent component
-import { BasketProvider } from '../lib/BasketContext';  // Import BasketProvider
-import Head from 'next/head';  // Import Head component from Next.js
+import Header from '../components/Header';          // Import the Header component
+import Footer from '../components/Footer';          // Import the Footer component
+import { useModal } from '../lib/modals';           // Import the useModal hook
+import CookieConsent from '../components/CookieConsent'; // Import the CookieConsent component
+import { BasketProvider } from '../lib/BasketContext';   // Import BasketProvider
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';                // Import Google Analytics tracking functions
 
 function MyApp({ Component, pageProps }) {
   const { isModalOpen, closeModal } = useModal(true);
+  const router = useRouter();
+
+  // Track page views when the route changes
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);  // Send pageview event to Google Analytics
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <BasketProvider>
       <div className="flex flex-col min-h-screen">
-
         {/* Header */}
         <Header />
 
@@ -20,7 +35,7 @@ function MyApp({ Component, pageProps }) {
         <main className="flex-grow flex flex-col items-center justify-center bg-gray-100">
           <Component {...pageProps} />
         </main>
-        
+
         {/* Footer */}
         <Footer />
 
--- app-main/pages/_document.js
+++ app-main/pages/_document.js
@@ -1,14 +1,40 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../lib/gtag';  // Import the GA Tracking ID
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        {GA_TRACKING_ID && (
+          <>
+            {/* Load the GA script */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            ></script>
+            {/* Initialize GA */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
         {/* Favicon links */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        {/* Google Maps API */}
         <script
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}`}
           async
