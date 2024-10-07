4506b372d540a1c96031daf4c476a88dd01f5534
Victor Reipur <victor.reipur@gmail.com>
2024-10-07 17:56:55 +0000

    try agains

---
+++ app-main/pages/payment-success.js
@@ -0,0 +1,9 @@
+export default function PaymentSuccess() {
+    const orderId = "1234567890";
+    return (
+        <div>
+            <h1>Payment Success</h1>
+            <h2>Order ID: {orderId}</h2>
+        </div>
+    );
+}
