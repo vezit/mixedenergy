590b00e11127f5011e48592a2184a2d6cc2b9e0d
Victor Reipur <victor.reipur@gmail.com>
2024-10-07 17:09:51 +0000

    firebaseAdmin lad os se om det virker i produktion

--- app-main/lib/firebaseAdmin.js
+++ app-main/lib/firebaseAdmin.js
@@ -2,47 +2,20 @@
 
 import admin from 'firebase-admin';
 
+// Parse the JSON string from the environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      // Your service account credentials
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\\\n/g, '\\n'),
+      projectId: serviceAccount.project_id,
+      clientEmail: serviceAccount.client_email,
+      privateKey: serviceAccount.private_key.replace(/\\n/g, '\n'),
     }),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`,
   });
 }
 
 const db = admin.firestore();
 
 export { admin, db };
-
-
-
-// // /lib/firebaseAdmin.js
-
-// import * as admin from 'firebase-admin';
-
-// let privatekey; // Declare privatekey
-
-// if (!admin.apps.length) {
-//   if (!process.env.FIREBASE_ADMIN_KEY) {
-//     throw new Error('FIREBASE_ADMIN_KEY is not defined in environment variables');
-//   }
-
-//   // Parse FIREBASE_ADMIN_KEY and handle newlines in the private key
-//   const serviceAccount = JSON.parse(
-//     process.env.FIREBASE_ADMIN_KEY.replace(/\\\\n/g, '\\n')
-//   );
-
-//   privatekey = serviceAccount.private_key;
-
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//   });
-// }
-
-// const db = admin.firestore();
-
-// export { db };
