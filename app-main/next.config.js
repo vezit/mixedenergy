58501afef84b4c08c9ce3801b3eb3777c9c5d101
Victor Reipur <victor.reipur@gmail.com>
2024-11-13 21:42:10 +0000

    cron cron cron

--- app-main/next.config.js
+++ app-main/next.config.js
@@ -1,6 +1,5 @@
 // next.config.js
 
-
 export default {
   images: {
     domains: ['firebasestorage.googleapis.com'],
@@ -10,6 +9,14 @@ export default {
       config.devtool = 'cheap-module-source-map';
     }
     return config;
+  },
+  // Function configuration
+  functions: {
+    // Key is the API route path relative to the 'pages' directory
+    'api/cron/deleteOldSessions': {
+      memory: 256,
+      maxDuration: 60,
+    },
   },
   // ...other configurations
 };
