e71034b8fd0cf6f66cf47f8ee91a4d51ec7319fd
Victor Reipur <victor.reipur@gmail.com>
2024-11-23 21:11:58 +0000

    delete old files

--- app-main/pages/api/old/sessionLogout.js
+++
@@ -1,8 +0,0 @@
-// pages/api/sessionLogout.js
-
-
-
-export default async (req, res) => {
-  res.setHeader('Set-Cookie', 'session=; Max-Age=0; Path=/; HttpOnly; Secure; SameSite=Strict');
-  res.status(200).json({ success: true });
-};
